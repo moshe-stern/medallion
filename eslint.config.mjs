39e5425d11890506dd357583bd0c2d53a9f52b46
moshe-stern <guntheramassed@gmail.com>
2024-11-26 16:52:06 -0500

    Test pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports';
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +10,21 @@ export default [
      { languageOptions: { globals: globals.browser } },
      pluginJs.configs.recommended,
      ...tseslint.configs.recommended,
+     {
+          plugins: {
+               'unused-imports': unusedImports,
+           },
+           rules: {
+               'unused-imports/no-unused-imports': 'error', // Automatically removes unused imports
+               'unused-imports/no-unused-vars': [
+                   'warn',
+                   {
+                       vars: 'all',
+                       varsIgnorePattern: '^_',
+                       args: 'after-used',
+                       argsIgnorePattern: '^_',
+                   },
+               ],
+           },
+     }
 ]
