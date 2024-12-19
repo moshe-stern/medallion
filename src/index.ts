d1ec91e67df103079f45cbe5b0960d0a687b494e
moshe-stern <guntheramassed@gmail.com>
2024-12-19 10:32:14 -0500

    prod

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { app } from '@azure/functions'
 import dotenv from 'dotenv'
 import medallionApi from '@api/medallion-api'
 dotenv.config()
-const apiKey = process.env.DEVELOPMENT ? process.env.API_KEY : process.env.PROD_API_KEY
+const apiKey = process.env.DEVELOPMENT
+     ? process.env.API_KEY
+     : process.env.PROD_API_KEY
 medallionApi.auth(apiKey as string)
 app.setup({
      enableHttpStream: true,
