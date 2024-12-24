794e09ba535855a61a475fe167ecbffef57ee312
moshe-stern <guntheramassed@gmail.com>
2024-12-24 12:03:00 -0500

    add auth

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { app } from '@azure/functions'
 import dotenv from 'dotenv'
 import medallionApi from '@api/medallion-api'
+import { auth } from 'attain-aba-shared'
 
 dotenv.config()
+auth(process.env.DB_API_KEY!)
 export const apiKey = process.env.DEVELOPMENT
      ? process.env.API_KEY
      : process.env.PROD_API_KEY
