4842128ef6d65b855d4b3dac05fe1edbe8e8f772
moshe-stern <guntheramassed@gmail.com>
2024-12-25 09:12:58 -0500

    group

---
+++ src/services/group-profiles.ts
@@ -0,0 +1,10 @@
+import medallionApi from '@api/medallion-api'
+
+export async function getGroupProfileIdByName(name?: string) {
+     const res = await medallionApi.p_api_v1_group_profiles_list_groupProfiles({
+          name,
+     })
+     const data = res.data.results
+     if (!data) return
+     return data[0].id
+}
